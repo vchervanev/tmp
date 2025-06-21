9658a809074909d4260eccb31867920cde472fd5
Vladimir Chervanev <vchervanev.dev@gmail.com>
2025-06-21 22:07:53 +0000

    cli_handler -> cli::hanlder

---
+++ app/services/cli/handler.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Cli
+  class Handler
+    attr_reader :db, :skipper, :state, :values, :echo
+
+    Error = Class.new(StandardError)
+
+    ORIGIN = 0
+    DESTINATION = 1
+    STRATEGY = 2
+
+    def initialize(db, echo: false)
+      @db = db
+      @skipper = Skipper.new(db)
+      @echo = echo
+    end
+
+    def take_one(io)
+      values = Array.new(3).map { io.readline.chomp }
+      output = execute(
+        values[ORIGIN],
+        values[DESTINATION],
+        values[STRATEGY].gsub('-', '_').to_sym
+      )
+
+      output = "#{values.join("\n")}\n#{output}" if echo
+
+      output
+    end
+
+    def execute(origin, destination, strategy)
+      journey = skipper.route(origin, destination, strategy:)
+      if journey
+        journey.sailings
+        Cli::Formatter.sailings(journey.sailings)
+      else
+        "No route found\n"
+      end
+      # rescue StandardError => e
+      # raise Error, e.message
+    end
+  end
+end
--- app/services/cli/runner.rb
+++ app/services/cli/runner.rb
@@ -5,13 +5,13 @@ module Cli
     def self.execute(input, output)
       db = Json::DbLoader.load('./json/map_reduce_response.json')
       echo = ARGV[0] == '--echo'
-      cli = CliHandler.new(db, echo:)
+      cli = Cli::Handler.new(db, echo:)
 
       until input.eof?
         begin
           response = cli.take_one($stdin)
           output.puts(response) if response
-        rescue CliHandler::Error => e
+        rescue Cli::Handler::Error => e
           warn(e.message)
         end
       end
